9b7e68b54a1b5908cf0e9a36b08364c4e178b70b
suru07jee <kuberasusu2006@gmail.com>
2026-09-17 14:15:06 +0530

    Add files via upload

---
+++ Experiment7a/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Windows.Forms;
+
+namespace Experiment_07
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
