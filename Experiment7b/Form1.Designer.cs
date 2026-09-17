db3c0b60aaf5a33d523b062eb3b0620a250ca5c1
suru07jee <kuberasusu2006@gmail.com>
2026-09-17 14:17:40 +0530

    Add files via upload

---
+++ Experiment7b/Form1.Designer.cs
@@ -0,0 +1,30 @@
+﻿namespace StudentResult
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 250);
+            this.Name = "Form1";
+            this.Text = "Student Result";
+
+            this.ResumeLayout(false);
+        }
+    }
+}
